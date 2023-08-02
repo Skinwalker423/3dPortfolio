d17176987b89301c3c7c21ba3987cd50e71cda65
Luis Gonzalez <lgonzalez23@verizon.net>
2023-08-02 16:46:03 -0700

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import {
+  Hero,
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Tech,
+  Works,
+  StarsCanvas,
+  Navbar,
+} from "./components";
 
 function App() {
-  return <div>3d Portfolio</div>;
+  return (
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className='relative z-0'>
+        <Contact />
+        <StarsCanvas />
+      </div>
+
+      <Routes>
+        <Route path='/about' element={<About />} />
+        <Route
+          path='/experience'
+          element={<Experience />}
+        />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/feedbacks' element={<Feedbacks />} />
+        <Route path='/hero' element={<Hero />} />
+        <Route path='/tech' element={<Tech />} />
+        <Route path='/works' element={<Works />} />
+        <Route
+          path='/starscanvas'
+          element={<StarsCanvas />}
+        />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

01517ec824e0d455dc567313499149a68e82459a
Luis Gonzalez <lgonzalez23@verizon.net>
2023-08-03 15:24:11 -0700

    logo / header styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,27 @@ import { logo, menu, close, artLogo } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState(null);
+  const [toggle, setToggle] = useState(false);
+
+  const navList = navLinks.map(({ title, id }) => {
+    return (
+      <li
+        key={id}
+        className={`${
+          active === id ? "text-orange-500" : "text-white"
+        } hover:text-orange-500 text-[18px] font-medium cursor-pointer ${
+          toggle && "font-poppins text-[16px]"
+        }`}
+        onClick={() => {
+          setActive(id);
+          setToggle(false);
+        }}
+      >
+        <Link to={`/${id}`}>{title}</Link>
+      </li>
+    );
+  });
+
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -26,7 +47,7 @@ const Navbar = () => {
             height={65}
             className='w-9 h-9 object-contain'
           />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex justify-center items-center gap-2'>
             Art Gonzalez
             <span className='sm:block hidden orange-text-gradient'>
               | Web Dev
@@ -34,23 +55,28 @@ const Navbar = () => {
           </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
-          {navLinks.map(({ title, id }) => {
-            return (
-              <Link
-                to={`/${id}`}
-                className={`${
-                  active === id
-                    ? "text-orange-500 border-b border-b-orange-500"
-                    : "text-white"
-                } hover:text-orange-500 text-[18px] font-medium cursor-pointer`}
-                onClick={() => setActive(id)}
-                key={id}
-              >
-                {title}
-              </Link>
-            );
-          })}
+          {navList}
         </ul>
+
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            className='w-[28px] h-[28px]'
+            src={toggle ? close : menu}
+            alt={
+              toggle ? "close icon" : "hamburger menu icon"
+            }
+            onClick={() => setToggle((bool) => !bool)}
+          />
+          <div
+            className={`${
+              toggle ? "flex" : "hidden"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className='list-none flex sm:hidden justify-end items-start flex-col gap-5'>
+              {navList}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );

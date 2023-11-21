e16edac7b59da0dbd3cf3e180a40575d620d740b
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-20 20:35:07 -0800

    about description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          Introduction
+        </p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I am a web developer with 3+ years of experience in
+        React, Next.js, CSS, HTML, and JavaScript. I have a
+        strong understanding of the fundamentals of web
+        development, and I am proficient in using a variety
+        of modern web development tools and frameworks. I am
+        also a highly motivated and results-oriented
+        individual with a proven track record of success.
+      </motion.p>
+    </>
+  );
 };
 
 export default About;

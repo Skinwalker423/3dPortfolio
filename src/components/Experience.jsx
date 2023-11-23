054393878cf5e1e40cf620789a5058cc1dfd2fa8
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-22 19:01:05 -0800

    Experience header

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+import "react-vertical-timeline-component/style.min.css";
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import SectionWrapper from "../hoc/SectionWrapper";
+import { textVariant } from "../utils/motion";
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          Development Journey
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Experience
+        </h2>
+      </motion.div>
+    </>
+  );
 };
 
-export default Experience;
+export default SectionWrapper(Experience, "work");

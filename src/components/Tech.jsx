5c8bf823a98130239934a15ce83c161dc9384c44
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-23 17:42:11 -0800

    Ball canvas list

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,7 +3,17 @@ import SectionWrapper from "../hoc/SectionWrapper";
 import { technologies } from "../constants";
 
 const Tech = () => {
-  return <div>Tech</div>;
+  return (
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((tech) => {
+        return (
+          <div className='w-28 h-28' key={tech.name}>
+            <BallCanvas icon={tech.icon} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default SectionWrapper(Tech, "tech");

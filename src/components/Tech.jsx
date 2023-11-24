b2c1279b29ee614ae22f54ad13a7f13586e44fdc
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-23 17:39:11 -0800

    section wrapper tech

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { BallCanvas } from "./canvas";
+import SectionWrapper from "../hoc/SectionWrapper";
+import { technologies } from "../constants";
 
 const Tech = () => {
   return <div>Tech</div>;
 };
 
-export default Tech;
+export default SectionWrapper(Tech, "tech");

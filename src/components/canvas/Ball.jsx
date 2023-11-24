35839e16ae7a185a65b84754a58af6ed98ec5068
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-23 17:50:17 -0800

    Ball canvas

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
 
-const Ball = () => {
+import CanvasLoader from "../Loader";
+
+const Ball = ({ imgUrl }) => {
+  const [decal] = useTexture(imgUrl);
+  return <div>Ball</div>;
+};
+
+const BallCanvas = ({ icon }) => {
   return (
-    <div>Ball</div>
-  )
-}
+    <Canvas
+      frameloop='demand'
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
 
-export default Ball
+export default BallCanvas;

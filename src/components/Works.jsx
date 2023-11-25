e5c6dfb1faab1480b6da033f4e82c0b0599838d8
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-24 18:13:17 -0800

    imports

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { github } from "../assets";
+import SectionWrapper from "../hoc/SectionWrapper";
+import { projects } from "../constants";
 
 const Works = () => {
   return <div>Works</div>;
 };
 
-export default Works;
+export default SectionWrapper(Works, "work");

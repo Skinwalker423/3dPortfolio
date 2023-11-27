41f9bcd0d59b413c28ffdafe174e35923c2f0f90
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-26 18:24:09 -0800

    contact header

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import SectionWrapper from "../hoc/SectionWrapper";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  function handleChange(e) {}
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+      <motion.div
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+        variants={slideIn("left", "tween", 0.2, 1)}
+      >
+        <p className={styles.sectionSubText}>
+          Get in Touch
+        </p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+      </motion.div>
+    </div>
+  );
 };
 
-export default Contact;
+export default SectionWrapper(Contact, "contact");

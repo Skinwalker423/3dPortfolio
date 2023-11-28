29953e4db84377f25649a6f3d5ad3342ad5e1e84
Luis Gonzalez <lgonzalez23@verizon.net>
2023-11-27 19:31:15 -0800

    stars canvas

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {
+  useState,
+  useEffect,
+  Suspense,
+} from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  Points,
+  Preload,
+  PointMaterial,
+} from "@react-three/drei";
+
+import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = () => {
-  return (
-    <div>Stars</div>
-  )
-}
+  return <div>Stars</div>;
+};
 
-export default Stars
+const StarsCanvas = () => {
+  return (
+    <div className='w-full h-full absolute inset-0 -z-10'>
+      <Canvas
+        camera={{
+          position: [0, 0, 1],
+        }}
+      >
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarsCanvas;
